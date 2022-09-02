845132dc2d8fb3832736ea5d313122ce6965bdac
Roni <ronniiii@yahoo.com>
2022-09-02 10:43:53 +0100

    v

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Info from "./Components/Info"
+import About from "./Components/About"
+import Intrests from "./Components/Intrests"
+import Footer from "./Components/Footer"
+
+function App() {
+  return (
+    <main>
+        <Info />
+        <About />
+        <Intrests />
+        <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Intrests.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <div className="deets">
+            <h4>Intrests</h4>
+            <p className="text">
+                Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
+        </div>
+    )
+}
